7f9dbb0fcb4ced89dea4b58f538712d20af2d33e
Mani Kumar <purammanikumar@gmail.com>
2021-01-21 14:39:02 -0500

    no changes made

---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    // state = {  }
+    render() { 
+        React.createElement('div')
+        return <div>
+            <h1>Hello World</h1><button>hhh</button>
+            <h2>Hello World</h2><button>hhh</button>
+            <h4>Hello World</h4><button>hhh</button>
+            <h6>Hello World</h6><button>hhh</button>
+            
+            </div>
+    }
+}
+ 
+export default Counter;
