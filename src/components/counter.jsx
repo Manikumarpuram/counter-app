05def1009a808c9ba0d10fe0ed0c6c57d92399e1
Mani Kumar <purammanikumar@gmail.com>
2021-01-22 12:34:08 -0500

    made some changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-    // state = {  }
+    // state = {  
+    //     count: 0
+    // };
     render() { 
         React.createElement('div')
         return <div>
+
             <h1>Hello World</h1><button>hhh</button>
-            <h2>Hello World</h2><button>hhh</button>
-            <h4>Hello World</h4><button>hhh</button>
-            <h6>Hello World</h6><button>hhh</button>
+        </div>
+           
             
-            </div>
-    }
+    
+    };
 }
  
 export default Counter;
