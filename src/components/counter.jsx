4ac38d4191dc2052aa08ae23a49524054428c16c
Mani Kumar <purammanikumar@gmail.com>
2021-01-24 20:04:29 +0530

    using from another

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-    // state = {  
-    //     count: 0
-    // };
+    state = {  
+        count: 0
+    };
     render() { 
         React.createElement('div')
-        return <div>
+        return (
+            <span className = {this.state.formatCount()}></span>
 
             <h1>Hello World</h1><button>hhh</button>
-        </div>
+        
+        );
+        
+
+
+        formatCount () {
+            const{count} = this.state;
+            return {count} === 0 ? "zero" : count;
+        }
            
             
     
